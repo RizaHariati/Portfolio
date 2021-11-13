1f089e06dabc7549e47cbcf2a01ec055d9392d9a
RizaHariati <rizahariati@yahoo.com>
2021-11-13 16:28:39 +0700

    typo repair

--- src/data.js
+++ src/data.js
@@ -5,7 +5,7 @@ const projects = [
     type: "Website",
     title: "RS. Urip Sumoharjo",
     text: [
-      "A website design for a local hospital in Bandar Lampung. Inspired by Mayo Clinic's website. A very simple design, easy-to-use, informative but not too crowded with too many useless details",
+      "A website design for a local hospital in Bandar Lampung. Inspired by Mayo Clinic's website. A very simple design, easy-to-use, informative but not too crowded with too many useless details.",
       " The core of this website design is ReactJS, supported by Formik, react-router-DOM, and Yup",
     ],
     images: ["urip-website", "urip-tablet", "urip-phone"],
@@ -16,8 +16,8 @@ const projects = [
     type: "Website",
     title: "Azribook",
     text: [
-      "This website is based on Facebook’s website design, to showcase my ability in accessing and processing data with Rest API ",
-      "The data is thansk to Dummyapi.io for providing free ready to use API Service full with dummy data ",
+      "This website is based on Facebook’s website design, to showcase my ability in accessing and processing data with Rest API. ",
+      "The data is thanks to Dummyapi.io whose providing free ready to use API Service filled with dummy data. ",
       " I’m using React JS as the core of this web design with the help of Axios, Figma, and React router DOM ",
     ],
     images: ["azribook-website", "azribook-tablet", "azribook-phone"],
